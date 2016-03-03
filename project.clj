38ff147ff8fde73b8ce538d89ebf6be28aae37da
Takashi AOKI <me@tak.sh>
2016-03-03 06:49:30 +0000

    add test cases

--- project.clj
+++ project.clj
@@ -10,6 +10,9 @@
                    :dependencies [[org.clojure/clojure "1.8.0"]
                                   [thunknyc/profile "0.5.2"]]
                    :jvm-opts ["-Xmx2048m"]}
+             :test {:resource-paths ["test-resources"]
+                    :plugins [[lein-midje "3.2"]]
+                    :dependencies [[midje "1.8.3"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
---
+++ test/gugus/t_core.clj
@@ -0,0 +1,16 @@
+(ns gugus.t-core
+  (:require [midje.sweet :refer :all]
+            [clojure.java.io :as io]
+            [gugus.io :refer [read-pairs]]
+            [gugus.core :as core]))
+
+(def sample-result {:q 0.420654296875
+                    :communities {"LLL" #{"III" "KKK" "JJJ" "LLL" "GGG" "HHH"}
+                                  "FFF" #{"BBB" "DDD" "EEE" "FFF" "CCC" "AAA"}}})
+
+(fact "cluster mm"
+  (let [f (-> "sample.pairs"
+              io/resource
+              io/file
+              .getPath)]
+    (core/cluster-mm (read-pairs f))) => sample-result)

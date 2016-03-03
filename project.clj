207a4dd292a4b2f7f323c851296d4c9e47ad0c42
Takashi AOKI <me@tak.sh>
2016-03-03 06:55:15 +0000

    unspport clojure 1.5 and 1.6

--- project.clj
+++ project.clj
@@ -13,8 +13,6 @@
              :test {:resource-paths ["test-resources"]
                     :plugins [[lein-midje "3.2"]]
                     :dependencies [[midje "1.8.3"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}}
   :main gugus.cli)
